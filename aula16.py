0854c0b0a25031af1fe78a9ed4b401aa0ee2f27f
Lucas <97757814+lucaslukao@users.noreply.github.com>
2022-10-25 16:35:31 -0300

    Atualizações

--- aula16.py
+++ aula16.py
@@ -1,18 +1,24 @@
 lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim', 'Batata frita')
 num = (1, 5, 5, 9, 3, 7)
-#print(lanche)
-#for cont in range(0, len(lanche)):
-#    print(lanche[cont])
-
-#for pos, comida in enumerate(lanche):
-#   print(comida, pos)
-#for comida in sorted(num):
-#   print(sorted(lanche))
-# a = (2, 5, 4)
-# b = (5, 8, 1, 2)
-# c = a + b    # A ordem importa
-# print(c)
-# print(c.index(5, 2))
-pessoa = ('lucas', 17, 1.83, 'M')
-del(pessoa)
-print(pessoa)
+# print(lanche)
+# for cont in range(0, len(lanche)):
+#     print(lanche[cont])
+########################################
+# for pos, comida in enumerate(lanche):
+#   print(pos, '-', comida)
+####################################
+# print(sorted(num))
+# for pos, comida in enumerate(sorted(num)):
+#   print(sorted(num)[pos], '-', sorted(lanche))
+############################################
+a = (2, 5, 4)
+b = (5, 8, 1, 2)
+c = a + b    # A ordem importa
+print(c)
+print(c.index(5, 2))
+# .index() mostra a posição de um determinado valor, se colocar a vírgula e o outro valor vai mostrar a partir desse
+# valor.
+######################################
+# pessoa = ('lucas', 17, 1.83, 'M')
+# del pessoa
+# print(pessoa)
