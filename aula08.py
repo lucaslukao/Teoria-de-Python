f9846eccb9564bbf70f686a2c0086f3ac8cb34ef
Lucas <lm1955435@gmail.com>
2023-03-10 09:58:00 -0300

    Atualizações

--- aula08.py
+++ aula08.py
@@ -1,3 +1,9 @@
-from math import sqrt, floor
-n = float(input('Digite um número: '))
-print(f'A raiz quadrada é {sqrt(floor(n))}')
+# Biblioteca math
+# from math import sqrt, floor
+# n = float(input('Digite um número: '))
+# print(f'A raiz quadrada é {floor(sqrt(n))}')
+
+# Biblioteca random
+import random
+# n = random.randint(0, 10)
+print(random.randint(0, 2))
